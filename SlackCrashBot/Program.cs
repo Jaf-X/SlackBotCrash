2d15d79d3f613004248b771bfd67dd9d335c31d9
Anton Viklund <king-steam@hotmail.com>
2016-11-23 14:10:15 +0100

    Did some reworking.

--- SlackCrashBot/Program.cs
+++ SlackCrashBot/Program.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SlackAPI;
-using System.Net.NetworkInformation;
 using System.Collections;
 using System.Threading;
+using SlackAPI;
+
 
 namespace SlackCrashBot
 {
@@ -14,31 +14,13 @@ namespace SlackCrashBot
     {
         public string AUTHTOKEN = "xoxb-106585938448-78NFQiiNKP7IN23s1McXlc8V";
 
+
         Action messageRecived;
         public bool Status;
         public string IP = "localhost";
         Action<LoginResponse> connected;
-        
 
-        public static bool IsPingable(string nameOrAddress)
-        {
-            bool pingable = false;
-            Ping pinger = new Ping();
 
-          
-
-            try
-            {
-                PingReply reply = pinger.Send(nameOrAddress);
-                pingable = reply.Status == IPStatus.Success;
-            }
-            catch (PingException)
-            {
-                // Discard PingExceptions and return false;
-            }
-            return pingable;
-          
-        }
         public SlackSocketClient client;
 
 
@@ -50,32 +32,27 @@ namespace SlackCrashBot
 
         void Start(string[] args)
         {
-
-
-            
-
-            if (client == null)
-            {
-                client = new SlackSocketClient(AUTHTOKEN);
-            }
-
+            client = new SlackSocketClient(AUTHTOKEN);
+            // Create seperate thread for the head loop to prevent of Thread.Sleep
             Thread headloop = new Thread(HeadLoop);
-            
             headloop.Start();
-            Thread CheckStat = new Thread(() => CheckStatus(client));
-            CheckStat.Start();
+            //Thread CheckStat = new Thread(() => CheckStatus(client));
+            //CheckStat.Start();
+
+            //client.OnMessageReceived += (message) =>
+            //{
+            //    // Handle each message as you receive them
+            //    switch (message.text.ToLower())
+            //    {
+            //        case "":
+
+            //            break;
+            //    }
+            //};
 
             // If in case something needs to be handled via Direct Message commands.
-            client.OnMessageReceived += (message) =>
-            {
-                // Handle each message as you receive them
 
-
-
-
-            };
         }
-          
         void HeadLoop()
         {
             while (true)
@@ -91,44 +68,14 @@ namespace SlackCrashBot
                         //case "":
                         //    break;
                     }
-
-
-
-
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex);
                 }
-
-
             }
-
         }
-
-        void CheckStatus(SlackSocketClient client)
-        {
-           while (true) { 
-            if (IsPingable(IP))
-            {
-                Status = false;
-                Send("Server Down @ " + System.DateTime.Now.ToString(),client);
-                    Thread.Sleep(10000);
-                }
-            else
-            {
-                
-                Status = true;
-                Thread.Sleep(10000);
-            }
-
-            }
-          
-        }
-
-
-
-
+        
 
 
         void Send(string Message,SlackSocketClient client)
@@ -136,10 +83,20 @@ namespace SlackCrashBot
             connected = null;
             client.Connect(connected =>
             {
-                var user = client.Users.Find(x => x.name.Equals("antonviklund")); // you can replace slackbot with everyone else here
-                var dmchannel = client.DirectMessages.Find(x => x.user.Equals(user.id));
-                client.SendMessage(null, dmchannel.id, Message);
+                var c = client.Channels.Find(x => x.name.Equals("servercrashbot"));
+            client.SendMessage(null , c.id, Message);
 
+            });
+        }
+        void MessageToDevs(string Message, SlackSocketClient client)
+        {
+            connected = null;
+            client.Connect(connected =>
+            {
+
+                var user = client.Users.Find(x => x.name.Equals("")); 
+                var dmchannel = client.DirectMessages.Find(x => x.user.Equals(user.id));
+                client.SendMessage(null, dmchannel.id, "I don't know you yet!");
 
             });
         }
